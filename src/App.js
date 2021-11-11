14b1c8eef490e82ebda9db5858ec51da0bbc7b5b
Muhammad Zaim Abbasi <abbasizaim@yahoo.com>
2021-11-11 10:38:54 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import { Footer, Home, Topbar } from "./components/index";
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="topbar">
+        <Topbar />
+      </div>
+      <div className="main">
+        <Home />
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
+    </div >
   );
 }
 
---
+++ src/components/Topbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Topbar = () => {
+    return (
+        <h1>Topbar</h1>
+    );
+}
+
+export default Topbar;
